f345d5e1ff5ac9bbb2661d71dfb0b4c96b99eca0
Samudraneel Sarkar <samudraneel05@gmail.com>
2026-04-19 23:29:56 +0530

    initialization

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="smoldata",
+    version="0.1.0",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    python_requires=">=3.10",
+    description="Locality-Aware Initialization for ViTs on Small Datasets",
+)
