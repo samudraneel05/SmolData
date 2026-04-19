ba59139c921449d0fad004dde632a5d1eeab0b8b
Samudraneel Sarkar <samudraneel05@gmail.com>
2026-04-20 03:15:47 +0530

    fix init

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -5,12 +5,12 @@ from .vit_scratch import ViTScratch, vit_tiny, vit_small, vit_base_paper1
 from .resnet import SmallResNet, resnet18, resnet56
 from .sl_vit import SLViT
 from .life_module import LIFEProjection, LIFEAttention
-from .life_deit import LIFEDeiT, life_deit_tiny, life_deit_small
+from .life_deit import LIFEViT, life_deit_tiny, life_deit_small
 
 __all__ = [
     "ViTScratch", "vit_tiny", "vit_small", "vit_base_paper1",
     "SmallResNet", "resnet18", "resnet56",
     "SLViT",
     "LIFEProjection", "LIFEAttention",
-    "LIFEDeiT", "life_deit_tiny", "life_deit_small",
+    "LIFEViT", "life_deit_tiny", "life_deit_small",
 ]
